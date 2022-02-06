0ea5b99b9d0aa06507138e5b202359c713749a60
BaharSarlak <bsarlak98@gmail.com>
2022-02-05 21:47:06 -0500

    linked form posted data to server app

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const https=require('https');
 
 const app=express();
 
-app.listen(3000,()=> console.log('server running on port 3000'));
 app.use(express.static(__dirname + '/public'))
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/signup.html');
-    console.log('Response Status Code: '+res.statusCode)
+    console.log('Response Status Code: '+res.statusCode);
 })
+
+app.post('/', (req,res)=>{
+    const userInfo=req.body
+    const firstName=userInfo.firstname;
+    const lastName=userInfo.lastname;
+    const email=userInfo.email;
+    console.log(userInfo);
+
+})
+
+app.listen(3000,()=> console.log('server running on port 3000'));
