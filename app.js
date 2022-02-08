3589c0ca49a010e0e168262b19738d5f62e9ac22
BaharSarlak <bsarlak98@gmail.com>
2022-02-07 22:21:43 -0500

    added mailchimp

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express=require('express');
-const https=require('https');
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+const mailchimpCredentials=require('./mailchimpCredentials');
+// ********************************
+
+mailchimp.setConfig({
+  apiKey: mailchimpCredentials.apiKey,
+  server: mailchimpCredentials.serverPrefix
+});
+
+// ******************************8
 
 const app=express();
 
@@ -9,16 +18,56 @@ app.use(express.urlencoded());
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/signup.html');
-    console.log('Response Status Code: '+res.statusCode);
+    console.log('Response Status Code for get request to signup page: '+res.statusCode);
 })
 
 app.post('/', (req,res)=>{
     const userInfo=req.body
-    const firstName=userInfo.firstname;
-    const lastName=userInfo.lastname;
-    const email=userInfo.email;
-    console.log(userInfo);
+    console.log('User submitted these info: '+ JSON.stringify(userInfo))
 
+    // adding contact to our Mailchimp list:
+
+    // mailchimp list id:
+    const listId = mailchimpCredentials.listId;
+
+    // user info object:
+    const subscribingUser = {
+    firstName: userInfo.firstname,
+    lastName: userInfo.lastname,
+    email: userInfo.email
+    };
+
+    // function to add user to the list
+    async function addContact() {
+        const response = await mailchimp.lists.addListMember(listId, {
+            email_address: subscribingUser.email,
+            status: "subscribed",
+            merge_fields: {
+            FNAME: subscribingUser.firstName,
+            LNAME: subscribingUser.lastName
+            }
+        });
+
+        if(response.statusCode===200) {
+            console.log(
+                `Successfully added contact as an audience member. The contact's id is ${response.id}.`
+            );
+            res.sendFile(__dirname+'/success.html');
+        } else{
+            console.log(JSON.stringify(response))
+            console.log(
+                `Failded to add contact. response status code:${response.statusCode}`
+            );
+            res.sendFile(__dirname+'/fail.html');
+        }
+    }
+
+    addContact();
+
+})
+
+app.post('/Fail.html',(req,res)=>{
+    res.redirect('/')
 })
 
 app.listen(3000,()=> console.log('server running on port 3000'));
